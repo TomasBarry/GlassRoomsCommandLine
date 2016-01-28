4450fba12f5eafa5d0c2b94ea85b9de9717858a4
Tomas Barry <tomas.barry@hotmail.com>
2016-01-28 17:18:45 +0000

    Exit gracefully on auto booking

--- DatabaseController/__main__.py
+++ DatabaseController/__main__.py
@@ -47,80 +47,80 @@ if __name__ == "__main__":
 			data = generateBookingRequest(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8]))
 		added_header = fetch_request.add_header("Authorization", generateAuthorizationCode(sys.argv[9], sys.argv[10]))
 		contents = request.urlopen(fetch_request)
-		
+	else:
+		credentials_confirmed = False
 
-	credentials_confirmed = False
+		while credentials_confirmed is False:
 
-	while credentials_confirmed is False:
+			# get user credentials
+			username = input("Enter your username: ")
+			password = getpass.getpass("Enter your password: ")
 
-		# get user credentials
-		username = input("Enter your username: ")
-		password = getpass.getpass("Enter your password: ")
-
-		try:
-			fetch_request = request.Request(Constants.URL_HEADER + str(1) + Constants.URL_ENDER )
-			added_header = fetch_request.add_header("Authorization", generateAuthorizationCode(username, password))
-			contents = request.urlopen(fetch_request)
-			credentials_confirmed = True
-		except error.HTTPError:
+			try:
+				fetch_request = request.Request(Constants.URL_HEADER + str(1) + Constants.URL_ENDER )
+				added_header = fetch_request.add_header("Authorization", generateAuthorizationCode(username, password))
+				contents = request.urlopen(fetch_request)
+				credentials_confirmed = True
+			except error.HTTPError:
 				print("User credentials were incorrect")
 
-	# establish connection to database
-	sql_location = Constants.DATABASE_NAME
-	conn = sqlite3.connect(sql_location)
-	c = conn.cursor()
+		# establish connection to database
+		sql_location = Constants.DATABASE_NAME
+		conn = sqlite3.connect(sql_location)
+		c = conn.cursor()
 
-	UpdateDatabase(c, Constants.TABLE_NAME, username, password)
-	# Committing changes and closing the connection to the database file
-	conn.commit()
-	conn.close()
+		UpdateDatabase(c, Constants.TABLE_NAME, username, password)
+		# Committing changes and closing the connection to the database file
+		conn.commit()
+		conn.close()
 
-	today = datetime.now()
-	take = True
-	while take is True:
-		# get user input
-		command = (input("Enter a command (type 'help' for command options):")).lower()
+		today = datetime.now()
+		take = True
+		while take is True:
+			# get user input
+			command = (input("Enter a command (type 'help' for command options):")).lower()
 
-		if command == "update":
-			conn = sqlite3.connect(sql_location)
-			c = conn.cursor()
-			UpdateDatabase(c, Constants.TABLE_NAME, username, password)
-			# Committing changes and closing the connection to the database file
-			conn.commit()
-			conn.close()
-			print("Updated database")
+			if command == "update":
+				conn = sqlite3.connect(sql_location)
+				c = conn.cursor()
+				UpdateDatabase(c, Constants.TABLE_NAME, username, password)
+				# Committing changes and closing the connection to the database file
+				conn.commit()
+				conn.close()
+				print("Updated database")
 
-		elif command == "help":
-			print(command)
+			elif command == "help":
+				print(command)
+	
+			elif command == "today":
+				room = (input("What Room?")).lower()
+				conn = sqlite3.connect(sql_location)
+				c = conn.cursor()
+				if room == "all":
+					c.execute("SELECT Room, StartTime, EndTime, Message FROM {table_name} WHERE Date = {today}".format(table_name = Constants.TABLE_NAME, today = today.day))
+				else:
+					c.execute("SELECT Room, StartTime, EndTime, Message FROM {table_name} WHERE Date = {today} AND Room = {room_number}".format(table_name = Constants.TABLE_NAME, today = today.day, room_number = room))	
+				for booking in c.fetchall():
+					print("| Room: " + booking[0] + " | From: " + booking[1] + " | To " + booking[2] + " | - booked")
+				# Committing changes and closing the connection to the database file
+				conn.commit()
+				conn.close()
 
-		elif command == "today":
-			room = (input("What Room?")).lower()
-			conn = sqlite3.connect(sql_location)
-			c = conn.cursor()
-			if room == "all":
-				c.execute("SELECT Room, StartTime, EndTime, Message FROM {table_name} WHERE Date = {today}".format(table_name = Constants.TABLE_NAME, today = today.day))
+			elif command == "book":
+				room = (input("What Room?")).lower()
+				start_time = input("From:")
+				end_time = input("To:")
+				name = input("What is your first name?")
+				status = "ba3"
+				date = input("Day:")
+				month = input("Month:")
+				year = "1"
+				fetch_request = request.Request(Constants.URL_HEADER + str(room) + Constants.URL_BOOKING + Constants.URL_ENDER, data = generateBookingRequest(start_time, end_time, name, status, date, month, year))
+				added_header = fetch_request.add_header("Authorization", generateAuthorizationCode(username, password))
+				contents = request.urlopen(fetch_request)
+	
+			elif command == "quit":
+				take = False
 			else:
-				c.execute("SELECT Room, StartTime, EndTime, Message FROM {table_name} WHERE Date = {today} AND Room = {room_number}".format(table_name = Constants.TABLE_NAME, today = today.day, room_number = room))	
-			for booking in c.fetchall():
-				print("| Room: " + booking[0] + " | From: " + booking[1] + " | To " + booking[2] + " | - booked")
-			# Committing changes and closing the connection to the database file
-			conn.commit()
-			conn.close()
+				print("No such command '" + command + "' found")
 
-		elif command == "book":
-			room = (input("What Room?")).lower()
-			start_time = input("From:")
-			end_time = input("To:")
-			name = input("What is your first name?")
-			status = "ba3"
-			date = input("Day:")
-			month = input("Month:")
-			year = "1"
-			fetch_request = request.Request(Constants.URL_HEADER + str(room) + Constants.URL_BOOKING + Constants.URL_ENDER, data = generateBookingRequest(start_time, end_time, name, status, date, month, year))
-			added_header = fetch_request.add_header("Authorization", generateAuthorizationCode(username, password))
-			contents = request.urlopen(fetch_request)
-
-		elif command == "quit":
-			take = False
-		else:
-			print("No such command '" + command + "' found")
